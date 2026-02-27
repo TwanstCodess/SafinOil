09f0777b951ec0d37378b7c6337bd41795d77c94
TwanstCodess <sulltantwanst@gmail.com>
2026-02-27 20:44:57 +0300

    ads validation

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,11 +7,18 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+// user Admin Dashboard
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
+              public function canAccessPanel(Panel $panel): bool
+{
+    return $this->is_admin === 1;
+}
     /**
      * The attributes that are mass assignable.
      *
