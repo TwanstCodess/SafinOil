92031ab018128ba41bcfb63384de177f762a7fce
TwanstCodess <sulltantwanst@gmail.com>
2026-02-28 13:37:27 +0300

    ads

--- app/Filament/Resources/CreditPaymentResource/Pages/CreateCreditPayment.php
+++ app/Filament/Resources/CreditPaymentResource/Pages/CreateCreditPayment.php
@@ -9,4 +9,9 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateCreditPayment extends CreateRecord
 {
     protected static string $resource = CreditPaymentResource::class;
+
+         protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
