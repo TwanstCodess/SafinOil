de1b03d6bce104cf172d6d34ee5c394b6e1fb580
TwanstCodess <sulltantwanst@gmail.com>
2026-03-03 01:15:31 +0300

    ads

--- app/Filament/Resources/QuickSaleResource/Pages/ListQuickSales.php
+++ app/Filament/Resources/QuickSaleResource/Pages/ListQuickSales.php
@@ -8,6 +8,7 @@ use Filament\Resources\Pages\ListRecords;
 use Filament\Tables\Actions\BulkAction;
 use Illuminate\Database\Eloquent\Collection;
 use Filament\Notifications\Notification;
+use Carbon\Carbon;
 
 class ListQuickSales extends ListRecords
 {
@@ -21,15 +22,22 @@ class ListQuickSales extends ListRecords
                 ->icon('heroicon-m-plus')
                 ->color('success'),
 
-            Actions\Action::make('export')
-                ->label('هەناردەکردن')
-                ->icon('heroicon-m-arrow-down-tray')
+            Actions\Action::make('today_report')
+                ->label('ڕاپۆرتی ئەمڕۆ')
+                ->icon('heroicon-m-document-text')
                 ->color('info')
                 ->action(function () {
-                    Notification::make()
-                        ->title('ئەم تایبەتمەندیە لە داهاتوودا زیاد دەکرێت')
-                        ->warning()
-                        ->send();
+                    $date = Carbon::now()->format('Y-m-d');
+                    return redirect()->to(QuickSaleResource::getUrl('index', ['tableFilters[sale_date][single_date]' => $date]));
+                }),
+
+            Actions\Action::make('tomorrow_report')
+                ->label('ڕاپۆرتی سبەینێ')
+                ->icon('heroicon-m-document-text')
+                ->color('warning')
+                ->action(function () {
+                    $date = Carbon::now()->addDay()->format('Y-m-d');
+                    return redirect()->to(QuickSaleResource::getUrl('index', ['tableFilters[sale_date][single_date]' => $date]));
                 }),
         ];
     }
@@ -43,11 +51,22 @@ class ListQuickSales extends ListRecords
                 ->action(function (Collection $records) {
                     $totalAmount = $records->sum('total_amount');
                     $totalLiters = $records->sum('total_liters');
+                    $count = $records->count();
 
                     Notification::make()
                         ->title('کۆی گشتی دیاریکراوەکان')
-                        ->body('دینار: ' . number_format($totalAmount) . ' - لیتر: ' . number_format($totalLiters))
+                        ->body("ژمارە: {$count} شەفت - دینار: " . number_format($totalAmount) . ' - لیتر: ' . number_format($totalLiters))
                         ->info()
+                        ->send();
+                }),
+
+            BulkAction::make('print_selected')
+                ->label('چاپکردنی دیاریکراوەکان')
+                ->icon('heroicon-m-printer')
+                ->action(function (Collection $records) {
+                    Notification::make()
+                        ->title('ئەم تایبەتمەندیە لە داهاتوودا زیاد دەکرێت')
+                        ->warning()
                         ->send();
                 }),
         ];
