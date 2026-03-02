b0df3d0eb8ddf7660ac4a9fc377e85f857094da8
TwanstCodess <sulltantwanst@gmail.com>
2026-03-02 23:56:15 +0300

    ادس

--- app/Filament/Resources/QuickSaleResource/Pages/ListQuickSales.php
+++ app/Filament/Resources/QuickSaleResource/Pages/ListQuickSales.php
@@ -1,5 +1,5 @@
 <?php
-
+// app/Filament/Resources/QuickSaleResource/Pages/ListQuickSales.php
 namespace App\Filament\Resources\QuickSaleResource\Pages;
 
 use App\Filament\Resources\QuickSaleResource;
@@ -13,7 +13,9 @@ class ListQuickSales extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            Actions\CreateAction::make()
+                ->label('فرۆشی خێرای نوێ')
+                ->icon('heroicon-m-plus'),
         ];
     }
 }
