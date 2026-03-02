2d65c676cf531c58d52832ece6fa4d1b4ebee678
TwanstCodess <sulltantwanst@gmail.com>
2026-03-02 22:15:33 +0300

    ادس

--- app/Filament/Resources/QuickSaleResource/Pages/CreateQuickSale.php
+++ app/Filament/Resources/QuickSaleResource/Pages/CreateQuickSale.php
@@ -1,12 +1,30 @@
 <?php
-
+// app/Filament/Resources/QuickSaleResource/Pages/CreateQuickSale.php
 namespace App\Filament\Resources\QuickSaleResource\Pages;
 
 use App\Filament\Resources\QuickSaleResource;
-use Filament\Actions;
+use App\Models\QuickSale;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Support\Facades\Auth;
 
 class CreateQuickSale extends CreateRecord
 {
     protected static string $resource = QuickSaleResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $data['created_by'] = Auth::id();
+        $data['status'] = 'open';
+
+        // پڕکردنەوەی categories_data
+        $data['categories_data'] = QuickSale::getCategoriesGroupedByType();
+
+        return $data;
+    }
+
+    protected function afterCreate(): void
+    {
+        // حسابکردنی فرۆشراوەکان و جیاوازی
+        $this->record->calculateAll();
+    }
 }
