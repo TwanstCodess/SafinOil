d149c9d58ddb4a2b665a9219b1cd5fed10deabdb
TwanstCodess <sulltantwanst@gmail.com>
2026-03-02 22:17:20 +0300

    ادس

--- app/Filament/Resources/QuickSaleResource/Pages/CreateQuickSale.php
+++ app/Filament/Resources/QuickSaleResource/Pages/CreateQuickSale.php
@@ -3,9 +3,10 @@
 namespace App\Filament\Resources\QuickSaleResource\Pages;
 
 use App\Filament\Resources\QuickSaleResource;
-use App\Models\QuickSale;
 use Filament\Resources\Pages\CreateRecord;
 use Illuminate\Support\Facades\Auth;
+use App\Models\QuickSale;
+use App\Models\Category;
 
 class CreateQuickSale extends CreateRecord
 {
@@ -13,8 +14,18 @@ class CreateQuickSale extends CreateRecord
 
     protected function mutateFormDataBeforeCreate(array $data): array
     {
+        // پڕکردنەوەی created_by
         $data['created_by'] = Auth::id();
         $data['status'] = 'open';
+
+        // پڕکردنەوەی reported_sold بە دیفۆڵت
+        if (!isset($data['reported_sold'])) {
+            $data['reported_sold'] = [];
+            $categories = Category::all();
+            foreach ($categories as $category) {
+                $data['reported_sold'][$category->id] = 0;
+            }
+        }
 
         // پڕکردنەوەی categories_data
         $data['categories_data'] = QuickSale::getCategoriesGroupedByType();
@@ -26,5 +37,16 @@ class CreateQuickSale extends CreateRecord
     {
         // حسابکردنی فرۆشراوەکان و جیاوازی
         $this->record->calculateAll();
+
+        // نیشاندانی پەیامی سەرکەوتن
+        \Filament\Notifications\Notification::make()
+            ->title('فرۆشی خێرا بە سەرکەوتوویی تۆمارکرا')
+            ->success()
+            ->send();
+    }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
     }
 }
---
+++ app/Filament/Resources/QuickSaleResource/Pages/ViewQuickSale.php
@@ -0,0 +1,23 @@
+<?php
+// app/Filament/Resources/QuickSaleResource/Pages/ViewQuickSale.php
+namespace App\Filament\Resources\QuickSaleResource\Pages;
+
+use App\Filament\Resources\QuickSaleResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ViewRecord;
+
+class ViewQuickSale extends ViewRecord
+{
+    protected static string $resource = QuickSaleResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\Action::make('back')
+                ->label('گەڕانەوە')
+                ->url(fn (): string => QuickSaleResource::getUrl('index'))
+                ->icon('heroicon-m-arrow-right')
+                ->color('gray'),
+        ];
+    }
+}
